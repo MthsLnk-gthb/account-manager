6b08fc4fd7530a62771a8696fcfda0493ff9055d
Matheus Link <linksauermths2203@gmail.com>
2024-03-18 20:27:28 -0300

    Adicionando arquivos das telas de login e cadastro

---
+++ src/screens/LoginScreen/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const LoginScreen = () => {
+
+  return (
+    <View style={{ marginTop: 60 }}>
+      <Text style={{ textAlign: "center" }}>LoginScreen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
---
+++ src/screens/SignUpScreen/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SignUpScreen = () => {
+  return (
+    <View style={{ marginTop: 60 }}>
+      <Text style={{ textAlign: "center" }}>SignUpScreen</Text>
+    </View>
+  );
+};
+
+export default SignUpScreen;
